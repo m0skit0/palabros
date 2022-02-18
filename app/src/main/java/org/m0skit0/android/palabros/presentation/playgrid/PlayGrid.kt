a4dd1a23fa402f98271bfcadda6347c8a7addbc8
m0skit0 <ametric@gmail.com>
2022-02-18 21:10:54 +0100

    Added help button on play grid

--- app/src/main/java/org/m0skit0/android/palabros/presentation/playgrid/PlayGrid.kt
+++ app/src/main/java/org/m0skit0/android/palabros/presentation/playgrid/PlayGrid.kt
@@ -3,15 +3,17 @@ package org.m0skit0.android.palabros.presentation.playgrid
 import android.content.Context
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import org.m0skit0.android.palabros.R
@@ -20,6 +22,17 @@ import org.m0skit0.android.palabros.di.koin
 import org.m0skit0.android.palabros.presentation.Loading
 import org.m0skit0.android.palabros.presentation.toast
 import org.m0skit0.android.palabros.state.PlayGridState
+import org.m0skit0.android.palabros.theme.HelpSymbolColor
+
+@ExperimentalFoundationApi
+@Preview
+@Composable
+fun PlayGridPreview() {
+    PlayGridColumn(
+        onKeyClick = {},
+        playGridState = MutableStateFlow(PlayGridState())
+    )
+}
 
 @ExperimentalFoundationApi
 @Composable
@@ -37,18 +50,27 @@ fun PlayGrid(
 
 @ExperimentalFoundationApi
 @Composable
-private fun PlayGridColumn(onKeyClick: (Char) -> Unit) {
+private fun PlayGridColumn(
+    onKeyClick: (Char) -> Unit,
+    playGridState: Flow<PlayGridState> = koin.get<MutableStateFlow<PlayGridState>>(NAMED_PLAY_GRID_STATE_FLOW)
+) {
     Column(
         modifier = Modifier
             .background(MaterialTheme.colors.primary)
             .fillMaxSize()
     ) {
-        WordGrid()
+        HelpButton()
+        WordGrid(
+            playGridState = playGridState,
+        )
         Box(
             modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.BottomCenter
+            contentAlignment = Alignment.BottomCenter,
         ) {
-            Keyboard(onKeyClick = onKeyClick)
+            Keyboard(
+                onKeyClick = onKeyClick,
+                playGridState = playGridState,
+            )
         }
     }
 }
@@ -63,5 +85,22 @@ private fun PlayGridState.checkVictoryConditions(context: Context) {
 private fun PlayGridState.checkUnknownWord(context: Context) {
     if (isUnknownWord) {
         context.toast(R.string.word_not_found)
+    }
+}
+
+@Composable
+private fun HelpButton() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(end = 20.dp, top = 5.dp)
+            .clickable { },
+        contentAlignment = Alignment.CenterEnd,
+    ) {
+        Text(
+            style = MaterialTheme.typography.button,
+            color = HelpSymbolColor,
+            text = LocalContext.current.getString(R.string.help)
+        )
     }
 }
