18546f6622d67f4dd7f951b40cefc5e720124a3f
m0skit0 <ametric@gmail.com>
2022-02-26 20:41:41 +0100

    Added AbandonUseCase

---
+++ app/src/main/java/org/m0skit0/android/palabros/usecase/AbandonUseCase.kt
@@ -0,0 +1,14 @@
+package org.m0skit0.android.palabros.usecase
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import org.m0skit0.android.palabros.di.NAMED_PLAY_GRID_STATE_FLOW
+import org.m0skit0.android.palabros.di.koin
+import org.m0skit0.android.palabros.state.PlayGridState
+
+typealias AbandonUseCase = () -> Unit
+
+fun abandon(
+    state: MutableStateFlow<PlayGridState> = koin.get(NAMED_PLAY_GRID_STATE_FLOW)
+) {
+
+}
