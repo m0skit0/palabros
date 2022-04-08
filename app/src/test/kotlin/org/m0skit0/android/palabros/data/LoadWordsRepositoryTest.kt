20ff078be14d045e80fe3b76e7362d00291bdef9
m0skit0 <ametric@gmail.com>
2022-04-08 21:35:59 +0200

    Tests for LoadWordsRepository

--- app/src/test/kotlin/org/m0skit0/android/palabros/data/LoadWordsRepositoryTest.kt
+++ app/src/test/kotlin/org/m0skit0/android/palabros/data/LoadWordsRepositoryTest.kt
@@ -2,6 +2,7 @@ package org.m0skit0.android.palabros.data
 
 import android.content.Context
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.booleans.shouldBeFalse
 import io.mockk.*
 import kotlinx.coroutines.flow.MutableStateFlow
 import org.m0skit0.android.palabros.state.PlayGridState
@@ -19,17 +20,23 @@ class LoadWordsRepositoryTest : StringSpec({
 
     "when word dictionary is not empty on the play state, should do nothing" {
         val playGridState = PlayGridState(wordDictionary = listOf("one"))
-        val jsonReader = { _ : InputStream -> listOf("duh") }
+        var isJsonReaderCalled = false
+        val jsonReader = { _ : InputStream ->
+            isJsonReaderCalled = true
+            listOf("duh")
+        }
         every { mockContext.assets } returns mockk()
         every { mockMutableStateFlow.value } returns playGridState
         every { mockMutableStateFlow.value = any() } just Runs
 
         loadWordsRepository(mockContext, jsonReader = jsonReader, mockMutableStateFlow)
 
+        isJsonReaderCalled.shouldBeFalse()
+
         verify(inverse = true) {
             mockContext.assets
-            jsonReader(any())
         }
+
         verify {
             mockMutableStateFlow.value = playGridState
         }
